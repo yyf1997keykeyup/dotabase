700c271f3f89cac1e12c08b856d29e77806ddbdd
mrmitzh <tianzhanhong412@gmail.com>
2020-04-19 14:41:23 -0400

    add user register

--- frontend/src/layouts/Main.vue
+++ frontend/src/layouts/Main.vue
@@ -11,6 +11,7 @@
       </li>
       <li v-if="username === ''">
         <router-link :to="{name: 'login', params: {}}"> login </router-link>
+        <router-link :to="{name: 'register', params: {}}"> register </router-link>
       </li>
       <li v-else>
         <a> welcome, {{ username }}! Enjoy yourself or </a>
